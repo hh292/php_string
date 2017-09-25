3c1d1f29c70735d5991928016a404e9c29815f28
Himanshu Hunge <hh292@afsconnect1.njit.edu>
2017-09-25 15:29:18 -0400

    First string function

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+  $obj = new main();
+  $text_1 = "hh292@njit.edu";
+  $text_2 = "my name is himanshu hunge";
+  $obj->print_1($text_1);
+  //$obj->print_2($text_2);
+  class main {
+
+    public function __construct() {
+
+
+      echo '<h1>**********PHP Inbuilt String Functions ********** </h1>';
+
+    }
+   
+   // function 1 : strstr  ...hh292
+    
+    public function print_1($text_1) {
+
+      echo '<h2> Function Name : strstr </h2>'."\r";
+      echo 'Description: Find the first occurrence of a string</br>'."\r";	
+      echo 'Input --->'.$text_1."\r";
+      
+      $domain = strstr($text_1, '@');
+      echo '</br> 1) First output (If needle is not true) --->  '.$domain."\r";
+
+      $domain = strstr($text_1,'@',true);
+      echo '</br> 2) Second output (If needle is true) ---> '.$domain;
+
+    }
+    // function 2 : ucwords ...hh292
+   
+    /** public function print_2($text_2)
+    {
+      echo'<hr>';	
+      echo '<h2> Function Name : ucwords </h2>'."\r";
+      echo 'Description: Uppercase the first character of each word in a string </br>'."\r";
+      echo 'Input --->  '.$text_2."\r";
+     
+      $domain = ucwords($text_2);
+      echo '</br>Output --->  '.$domain."\r";
+      
+    }**/
+    
+    public function __destruct() {
+
+     echo '</br> <h3>Finally I\'m Done with string functions. </h3> </br>';
+     echo '</br> ********************Copyright Assignment @Himanshu Hunge (hh292)***************************';
+  
+   } 
+  
+}
